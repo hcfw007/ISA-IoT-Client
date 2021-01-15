ed99d71232f3e3b0b219d51d23f1782751f21091
hcfw007 <brotherstyx@gmail.com>
2021-01-15 15:03:35 +0800

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/singleline-html-element-content-newline': 0,
+    'space-before-function-paren': 0,
+    'prefer-const': 0,
+    'template-curly-spacing': 0,
+    'vue/no-unused-vars': 1,
+    'no-unused-vars': 1,
+    'no-template-curly-in-string': 0
+  }
+}
---
+++ layouts/default.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container class="frame-container">
+    <el-main class="content">
+      <Nuxt />
+    </el-main>
+  </el-container>
+</template>
+
+<style lang="stylus">
+.frame-container
+  width: 100vw
+  height: 100vh
+
+  h3.page-title
+    display: inline-block
+    font-size: 24px
+
+  .page-title-link
+    margin-left: 10px
+    font-size: 14px
+    color: rgb(63, 144, 247)
+    cursor: pointer
+
+  .pange-title-hint
+    margin-left: 10px
+    font-size: 12px
+    color: #666
+
+  .main-content
+    .el-row:not(:first-child)
+      margin-top: 20px
+
+  .content
+    position: relative
+</style>
