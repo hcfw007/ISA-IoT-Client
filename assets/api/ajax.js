778184ae0ed036e7608f8ee82063d69b8dc847e4
hcfw007 <brotherstyx@gmail.com>
2021-01-29 17:40:14 +0800

    feat: (testing) request anti-shake

--- assets/api/ajax.js
+++ assets/api/ajax.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Vue from 'vue'
 
 const baseURL = '/isa-api'
 const headers = {}
@@ -8,6 +9,32 @@ const instance = axios.create({
   headers,
   timeout: 5000
 })
+
+const requestList = []
+const CancelToken = axios.CancelToken
+const removeFlag = (flag) => {
+  requestList.splice(requestList.findIndex(item => item === flag), 1)
+}
+
+instance.interceptors.request.use(
+  (config) => {
+    config.cancelToken = new CancelToken((cancel) => {
+      let requestFlag = JSON.stringify(config.url) + JSON.stringify(config.data) + '&' + config.method
+      if (requestList.includes(requestFlag)) {
+        cancel('dense requests')
+        Vue.prototype.$toast('重复请求，请3秒后再试...', failureToastOption)
+      } else {
+        requestList.push(requestFlag)
+        setTimeout(() => removeFlag(requestFlag), 3000)
+      }
+    })
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 const successToastOption = {
   customCss: {
@@ -39,8 +66,9 @@ const getRequestFactory = url => async(vueObj, dataItem, params = {}, successToa
   })
 }
 
-export const getverifyImg = uuid =>
+export const getVerifyImg = uuid =>
   instance.get('/usrmng/user/register/picture-code', {
+    responseType: 'blob',
     params: {
       uuid
     }
